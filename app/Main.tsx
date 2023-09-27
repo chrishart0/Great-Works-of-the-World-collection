be3f77d776c96568d20340dc6119d06563abebd5
chrishart0 <christianphart0@gmail.com>
2023-09-27 13:10:34 -0400

    A start

--- app/Main.tsx
+++ app/Main.tsx
@@ -11,9 +11,132 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Great Books of the Western World as selected by Mortimer Adler
+          </h2>
+          <p>
+            This is an ongoing project to create a free e-book collection of all the{' '}
+            <em>The Great Books of the Western World</em>.{' '}
+          </p>
+          <p>
+            The Great Books of the Western World is a remarkable collection that encompasses a wide
+            spectrum of the seminal works shaping Western thought over several centuries. This
+            collection, meticulously curated, traverses diverse disciplines such as philosophy,
+            literature, science, and history, featuring the illustrious minds from ancient Greeks
+            like Aristotle and Plato to the more contemporary like Shakespeare and Freud. Through
+            these volumes, readers can journey across the intellectual tapestry of the West,
+            examining the evolving ideas and dialogues that have persistently driven human progress.
+            The collection is not merely an anthology of influential texts but an invitation to
+            engage with the fundamental questions and principles that continue to mold our modern
+            world. Through the Great Books, readers are ushered into a rich dialogue with the
+            thinkers whose inquiries have defined the contours of Western civilization, offering a
+            profound exploration into our collective past and a better understanding of the present.
+          </p>
+          <h2 className="text-2xl font-bold leading-8 tracking-tight">Ebooks</h2>
+          <p>Source: https://www.mpc.edu/academics/academic-divisions/humanities-division/programs-centers/about-the-great-books-program/great-books-list</p>
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            <li key="a" className="py-12">
+              <article>
+                <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                    {/* <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100"> */}
+                    Homer
+                    {/* </Link> */}
+                  </h2>
+                  <div className="space-y-5 xl:col-span-3">
+                    <div className="space-y-6">
+                      <p><b>Iliad:</b></p>
+                      <Link
+                        href="https://www.gutenberg.org/ebooks/6130"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link>
+                    </div>
+                    <div className="space-y-6">
+                      <p><b>Odyssey:</b></p>
+                      <Link
+                        href="https://www.gutenberg.org/ebooks/1727"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </li>
+            <li key="a" className="py-12">
+              <article>
+                <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                    {/* <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100"> */}
+                    Aeschylus, Sophocles, Euripides, Aristophanes
+                    {/* </Link> */}
+                  </h2>
+
+                  Sophocles. Plays
+
+                  Euripides. Plays
+
+                  Aristophanes. Plays
+                  <div className="space-y-5 xl:col-span-3">
+                    <div className="space-y-6">
+                      <p><b>Aeschylus. Plays:</b></p>
+                      <p>ToDo:</p>
+                      {/* <Link
+                        href="https://www.gutenberg.org/ebooks/6130"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link> */}
+                    </div>
+                    <div className="space-y-6">
+                      <p><b>Sophocles. Plays:</b></p>
+                      <p>ToDo:</p>
+                      {/* <Link
+                        href="https://www.gutenberg.org/ebooks/1727"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link> */}
+                    </div>
+                    <div className="space-y-6">
+                      <p><b>Euripides. Plays:</b></p>
+                      <p>ToDo:</p>
+                      {/* <Link
+                        href="https://www.gutenberg.org/ebooks/1727"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link> */}
+                    </div>
+                    <div className="space-y-6">
+                      <p><b>Aristophanes. Plays:</b></p>
+                      <p>ToDo:</p>
+                      {/* <Link
+                        href="https://www.gutenberg.org/ebooks/1727"
+                        className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                        aria-label={`Read "Project Gutenberg"`}
+                      >
+                        Project Gutenberg &rarr;
+                      </Link> */}
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </li>
+          </ul>
+        </div>
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h2 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
-          </h1>
+          </h2>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Great Works of the World Collection',
+  author: 'Great Works of the World Collection',
+  headerTitle: 'Great Works Collection',
+  description: 'A Collection of the Great Works of the World.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
